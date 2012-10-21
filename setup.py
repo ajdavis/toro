c3fd617a92c1c4e7aee3a45c649526a581c4fbe6
A. Jesse Jiryu Davis <jesse@10gen.com>
2012-10-21 17:14:01 -0400

    Fix setup

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-from distutils.core import setup
+from setuptools import setup
 
 classifiers = """\
 Intended Audience :: Developers
 License :: OSI Approved :: Apache Software License
 Development Status :: 4 - Beta
-Framework :: Tornado
 Natural Language :: English
 Programming Language :: Python :: 2
 Programming Language :: Python :: 2.5
@@ -15,17 +14,17 @@ Operating System :: Unix
 Programming Language :: Python
 """
 
-description = 'Synchronization primitives for Tornado coroutines'
+description = 'Synchronization primitives for Tornado coroutines.'
 
 setup(name='toro',
       version='0.1',
       packages=['toro'],
       description=description,
-      long_description=description,
       author='A. Jesse Jiryu Davis',
       author_email='ajdavis@cs.oberlin.edu',
-      url='https://github.com/ajdavis/toro/',
-      requires=['tornado (>=2.3)'],
+      url='http://github.com/ajdavis/toro/',
+      install_requires=['tornado >= 2.4.0'],
       license='http://www.apache.org/licenses/LICENSE-2.0',
       classifiers=filter(None, classifiers.split('\n')),
+      keywords='tornado coroutines semaphore mutex queue asynchronous',
 )
