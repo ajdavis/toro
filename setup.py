9d0f1aaf5040a72409ecccec1f7e61e8a59448d1
A. Jesse Jiryu Davis <jesse@mongodb.com>
2015-01-13 17:10:09 -0500

    Drop Python 3.1, add Python 3.4.

--- setup.py
+++ setup.py
@@ -24,9 +24,9 @@ Programming Language :: Python :: 2
 Programming Language :: Python :: 2.6
 Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
-Programming Language :: Python :: 3.1
 Programming Language :: Python :: 3.2
 Programming Language :: Python :: 3.3
+Programming Language :: Python :: 3.4
 Operating System :: MacOS :: MacOS X
 Operating System :: Unix
 Programming Language :: Python
