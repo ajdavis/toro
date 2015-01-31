1b7e38039277944249f4ec614e735ea0e01000e0
A. Jesse Jiryu Davis <jesse@mongodb.com>
2015-01-31 11:03:55 -0500

    Explain future_with_timeout.

--- toro/_util.py
+++ toro/_util.py
@@ -9,6 +9,12 @@ null_future.set_result(None)
 
 
 def future_with_timeout(deadline, future, io_loop):
+    """Wrap Future with a timeout.
+
+    Unlike gen.with_timeout, the wrapped Future's exception is set by a timeout.
+    Queue, etc., need this so that consume_expired_waiters skips Futures whose
+    timeout wrappers have expired.
+    """
     if deadline is not None:
         result = gen.with_timeout(deadline, future, io_loop=io_loop)
         gen.chain_future(result, future)
